c1f84725a9e9f20f71d371db1d55c04faf39760b
better1514 <113873034+better1514@users.noreply.github.com>
2022-09-20 16:32:15 +0900

    [Bronze V] Title: A/B, Time: 228 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+            Scanner ac = new Scanner(System.in);
+
+
+            double a = ac.nextDouble();
+            double b = ac.nextDouble();
+
+            System.out.println(a/b);
+        }
+    }
