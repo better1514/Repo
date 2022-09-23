6d9ed5eb51b8bbab813bf9a72ab3c073f68ec482
better1514 <113873034+better1514@users.noreply.github.com>
2022-09-23 22:20:08 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -8,14 +8,10 @@ public class Main {
         int C = sc.nextInt();
         
         sc.close();
-
-        if(A>=2 && B>=2 && C>=2 && A<=10000 && B<=10000 && C<=10000) {
-        //(2 ≤ A, B, C ≤ 10000)
+        
         System.out.println((A+B)%C);
         System.out.println(((A%C) + (B%C))%C);
         System.out.println((A*B)%C);
         System.out.println(((A%C)*(B%C))%C);
-        }
-
     }
 }
